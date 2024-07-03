8c28d8fdf23c74f765ae35f3cb0f4d7d4e4dc2ec
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2024-07-03 15:40:07 +0900

    Feat: add newCreate page and setting basic datepicker, select plugins https://notion.so/TSK1-18, https://notion.so/TSK1-19

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,38 @@
-export default function New() {
-  return <div>새예약 페이지 입니다.</div>;
+"use client";
+
+import { useEffect, useState } from "react";
+import "react-date-picker/dist/DatePicker.css";
+import dynamic from "next/dynamic";
+import Select from "react-select";
+
+const DatePicker = dynamic(() => import("react-date-picker"), { ssr: false });
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+const options2 = [
+  { value: "홍길동", label: "홍길동" },
+  { value: "고영희", label: "고영희" },
+  { value: "김철수", label: "김철수" },
+];
+
+export default function NewCreate() {
+  const [value, onChange] = useState<Value>(new Date());
+  useEffect(() => {
+    onChange(null);
+  }, []);
+  return (
+    <div>
+      <p>새 예약 페이지 입니다.</p>
+      <DatePicker onChange={onChange} value={value} />
+      <Select options={options} />
+      <Select options={options2} />
+    </div>
+  );
 }
