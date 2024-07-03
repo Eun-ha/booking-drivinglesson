f5d4084a31bb5c352966b3b578f5a768448d5c60
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2024-07-03 16:20:38 +0900

    add form layout https://notion.so/TSK1-17

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -22,17 +22,42 @@ const options2 = [
   { value: "김철수", label: "김철수" },
 ];
 
+async function onSubmit(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const response = await fetch("/api/submit", {
+    method: "POST",
+    body: formData,
+  });
+
+  // Handle response if necessary
+  const data = await response.json();
+  // ...
+}
+
 export default function NewCreate() {
   const [value, onChange] = useState<Value>(new Date());
   useEffect(() => {
     onChange(null);
   }, []);
   return (
-    <div>
-      <p>새 예약 페이지 입니다.</p>
-      <DatePicker onChange={onChange} value={value} />
-      <Select options={options} />
-      <Select options={options2} />
-    </div>
+    <form onSubmit={onSubmit}>
+      <label>
+        예약날짜 : <DatePicker onChange={onChange} value={value} />
+      </label>
+      <br></br>
+      <label>
+        예약시간 : <Select options={options} />
+      </label>
+      <br></br>
+      <label>
+        강사명 : <Select options={options2} />
+      </label>
+      <br></br>
+      <label>연수시간 : 3시간</label>
+      <br></br>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
