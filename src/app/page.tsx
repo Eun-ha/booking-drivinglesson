83360dcd7fbd58ad6fc658bc89f9e654be373797
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2024-08-29 13:08:20 +0900

    Ui: add main style https://notion.so/TSK1-50

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>메인 페이지 입니다!!</h1>
+    <div className="mt-10 text-center">
+      <p className="pb-5">운전연수예약 메인페이지 입니다!</p>
+      <Image
+        className="inline"
+        src="https://picsum.photos/200/300"
+        alt="random image"
+        width={200}
+        height={300}
+      />
     </div>
   );
 }
